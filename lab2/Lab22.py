e34f9c4322be4ab029682acb9b134ef4a006dd8f
DimaChesnokov <dc.dima2708@mail.ru>
2023-10-05 12:34:44 +0300

    метод боя

--- lab2/Lab22.py
+++ lab2/Lab22.py
@@ -1,6 +1,12 @@
-import library
+import library , random
 
 if __name__ == "__main__":
    Warior1 = library.Warrior()
    Warior2 = library.Warrior()
-   print(Warior1.health) 
+   
+   print(Warior1.health ) 
+
+   while Warior1.health > 0 and Warior2.health > 0:
+    attacker = random.choice([warrior1, warrior2])
+    defender = warrior2 if attacker == warrior1 else warrior1
+    attacker.attack(defender)
