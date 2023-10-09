bdbbac70e0cebee5d9d8b9885675f449c51411a4
DimaChesnokov <dc.dima2708@mail.ru>
2023-10-09 23:11:53 +0300

    Дополнение комментарий к коду

--- lab2/Lab22.py
+++ lab2/Lab22.py
@@ -1,12 +1,23 @@
 import library , random
 
 if __name__ == "__main__":
+   #Создаём два экземлпяра воина
    Warior1 = library.Warrior()
    Warior2 = library.Warrior()
    
-   print(Warior1.health ) 
+   #print(Warior1.health ) 
 
+   #цикл идёт: Если здоровье одного из игроков достигнет нуля или меньше, то игра закончится.
    while Warior1.health > 0 and Warior2.health > 0:
-    attacker = random.choice([warrior1, warrior2])
-    defender = warrior2 if attacker == warrior1 else warrior1
-    attacker.attack(defender)
+    #Случайно выбираем атакующего игрока
+    attacker = random.choice([warrior1, warrior2]) 
+    #Если атакующий игрок - это warrior1, то защищаться будет warrior2, и наоборот
+    defense = warrior2 if attacker == warrior1 else warrior1
+    #Атакующий атакует защищающего 
+    attacker.attack(defense)
+
+    # Определяем победителя
+   if warrior1.health > 0:
+       print("Воин 1 - победил")
+   else:
+       print("Воин -  2 победил!")
