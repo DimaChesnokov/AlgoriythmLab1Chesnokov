a0774aa2cce34960ce6a427d31104d76a3618a0a
DimaChesnokov <100202160+DimaChesnokov@users.noreply.github.com>
2023-11-08 16:23:08 +0300

    обнова
    странное явление

---
+++ lab3/ClassFunction.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class QuadraticFunction:
+    def __init__(self, k, m, l):
+        self.k = k
+        self.m = m
+        self.l = l
+
+    def calculate(self, x):
+        return self.k * x**2 + self.m * x + self.l
+    
+    
+
+   
+
+if __name__ == "__main__":
+     # Заданные значения
+    k = int(input("Введите значение K: "))
+    m = int(input("Введите значение m: "))
+    l = int(input("Введите значение l: "))
+    print("параметр 'a'  должен быть больше параметра 'b'")
+    a = int(input("Введите значение a: ")) #пример с задания: a = -1; b = 1 h = 0,05
+    b = int(input("Введите значение b: "))
+    if a > b: 
+        print("параметр 'a' не должен быть больше параметра 'b'")
+        exit()
+    h = float(input("Введите значение h: "))
+
+    # Создаем объект класса QuadraticFunction
+    quadratic_function = QuadraticFunction(k, m, l)
+
+     # Создаем списки для значений x и y
+    x_values = np.arange(a, b + h, h)
+    y_values = [quadratic_function.calculate(x) for x in x_values]
+    print("f(x) = kx^2 + mx + l")
+    # Вывод таблицы значений
+    print("x\tf(x)")
+    x = a
+    while x <= b:
+        y = quadratic_function.calculate(x)
+        print(f"{x:.2f}\t{y:.2f}")
+        x += h
+    
+     # Построение графика
+    plt.plot(x_values, y_values, label=f"f(x) = {k}x^2 + {m}x + {l}")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.title("График функции f(x)")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+        
+        
+        
+
+        
---
+++ lab3/GUI.py
@@ -0,0 +1,69 @@
+import sys
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QTableWidgetItem
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+import math
+
+# Загрузка интерфейса из файла GUI.ui
+class Ui(QtWidgets.QMainWindow):
+    
+    #Инициализация
+    def __init__(self):
+        
+        super(Ui, self).__init__()
+        uic.loadUi("GUI.ui", self)
+        
+        #Присвоение кнопкам методов
+        self.pushButton.clicked.connect(self.start)
+        # self.pushButton_clear.clicked.connect(self.Clear_Table)
+        
+        #Настройка таблицы для вывода и ввода команд
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setHorizontalHeaderLabels(("x","f(x)"))
+        self.tableWidget.setColumnWidth(0,100)
+        self.tableWidget.setColumnWidth(1,100)
+        
+
+    def start(self):
+        
+        a = float(self.lineEdit_a.text())
+        b = float(self.lineEdit_b.text())
+        h = float(self.lineEdit_h.text())
+        m = float(self.lineEdit_m.text())
+        l = float(self.lineEdit_l.text())
+        k = float(self.lineEdit_k.text())
+        
+       
+        rowCount = self.tableWidget.rowCount()
+        while(a<=b):
+    
+            result=k * pow(a, 2) + m * a + l
+            self.tableWidget.insertRow(rowCount)
+           
+            self.tableWidget.setItem(rowCount,0,QTableWidgetItem(str(b)))
+            self.tableWidget.setItem(rowCount,1,QTableWidgetItem(str(result)))
+            b = b - h
+        
+    #Метод для очистки таблицы
+    # def Clear_Table(self):
+        
+    #     # Очистить содержимое ячеек
+    #     self.tableWidget.clearContents()  
+        
+    #     # Установить количество строк в 0
+    #     self.tableWidget.setRowCount(0) 
+           
+    #Метод для изменения нумерации ячеек    
+    def rowsEdit(self):
+        for i in range(self.tableWidget.rowCount()):
+            item = QTableWidgetItem(str(i))
+            self.tableWidget.setVerticalHeaderItem(i, item)
+        
+#Настройка формы
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    window.show()
+    sys.exit(app.exec_())
